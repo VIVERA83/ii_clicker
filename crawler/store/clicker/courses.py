44428844e8455d95a0e20fff80a44190453ddfd9
Aleksey Sergievskiy <vivers83@yandex.ru>
2024-03-31 13:39:14 +0300

    add new classes

---
+++ crawler/store/clicker/courses.py
@@ -0,0 +1,96 @@
+from enum import Enum
+from typing import Union, Type
+from dataclasses import dataclass
+
+TRAINING_TYPE = Union[Type["DriverCourseType"], Type["DispatcherCourseType"], Type["MentorCourseType"]]
+
+
+class CourseType(Enum):
+    eco_full = "Эко вождение полный формат"
+    # эко вождение малый формат
+    eco_small = "Эко вождение малый формат"
+    # защитное вождение
+    protective_driving = "Защитное вождение"
+    # итоговый тест водителя экспедитора С ПД
+    final_test_c_pd = "Итоговый тест водителя экспедитора С ПД"
+    # итоговый тест водителя экспедитора E ПД
+    final_test_e_pd = "Итоговый тест водителя экспедитора E ПД"
+    # итоговый тест водителя экспедитора E ТД
+    final_test_e_td = "Итоговый тест водителя экспедитора E ТД"
+    # итоговый тест водителя диспетчера
+    final_test_dispatcher = "Итоговый тест водителя диспетчера"
+    # итоговый тест водителя наставника
+    final_test_mentor = "Итоговый тест водителя наставника"
+
+
+# обучение по рейтингу
+@dataclass
+class RatingCourseType:
+    doc_id = 6929760816126312392
+    # эко вождение полный формат
+    eco_full = 6924645368353934140
+    # эко вождение малый формат
+    eco_small = 6924643920544605730
+    # защитное вождение
+    protective_driving = 6961398061758971672
+
+
+# обучение по водителя новичка (малый, полный, ТД)
+@dataclass
+class DriverCourseType:
+    doc_id = 6929760465124534937
+    # эко вождение полный формат
+    eco_full = 6962058253449045800
+    # эко вождение малый формат
+    eco_small = 6962057914491621733
+    # защитное вождение
+    protective_driving = 6961397254143690746
+    # итоговый тест водителя экспедитора С ПД
+    final_test_c_pd = 6924580342161351460
+    # итоговый тест водителя экспедитора E ПД
+    final_test_e_pd = 6924577233538150324
+    # итоговый тест водителя экспедитора E ТД
+    final_test_e_td = 6924579316631872065
+
+
+# обучение по диспетчеру новичка
+@dataclass
+class DispatcherCourseType:
+    doc_id = 6929760181765629593
+    # эко вождение полный формат
+    eco_full = 6962058253449045800
+    # эко вождение малый формат
+    eco_small = 6962057914491621733
+    # защитное вождение
+    protective_driving = 6961397254143690746
+    # итоговый тест водителя диспетчера
+    final_test_dispatcher = 6924590130235968967
+
+
+# обучение наставника
+@dataclass
+class MentorCourseType:
+    doc_id = 6929761152024067205
+    # эко вождение полный формат
+    eco_full = 6962058253449045800
+    # эко вождение малый формат
+    eco_small = 6962057914491621733
+    # защитное вождение
+    protective_driving = 6961397254143690746
+    # итоговый тест водителя наставника
+    final_test_mentor = 6924653197752958377
+
+
+class TrainingCourse:
+
+    def __init__(self, training_type: TRAINING_TYPE, courses: list[CourseType]):
+        self.mode = "course"
+        self.training_type = training_type
+        self.courses = courses
+
+    def get_params(self, course: CourseType) -> dict:
+        return {
+            "mode": self.mode,
+            "doc_id": self.training_type.doc_id,
+            "object_id": getattr(self.training_type, course.name)
+        }
