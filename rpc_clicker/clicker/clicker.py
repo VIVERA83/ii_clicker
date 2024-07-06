f8b8a8aa197bfc50634e26b6d9750af51c8d93be
Aleksey Sergievskiy <vivers83@yandex.ru>
2024-07-06 22:40:33 +0300

    update

--- rpc_clicker/clicker/clicker.py
+++ rpc_clicker/clicker/clicker.py
@@ -46,14 +46,14 @@ class CourseClicker:
     @staticmethod
     def get_driver_options() -> Options:
         options = webdriver.ChromeOptions()
-        # options.add_argument("--window-size=1920,1080")
-        # options.add_argument("--no-sandbox")
-        # options.add_argument("--disable-gpu")
-        # options.add_argument("--disable-dev-shm-usage")
-        # options.add_argument("--headless=new")
-        # options.add_argument("--start-maximized")
-        # options.add_argument("--ignore-certificate-errors")
-        # options.add_argument("--allow-running-insecure-content")
+        options.add_argument("--window-size=1920,1080")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--headless=new")
+        options.add_argument("--start-maximized")
+        options.add_argument("--ignore-certificate-errors")
+        options.add_argument("--allow-running-insecure-content")
         user_agent = UserAgent(browsers=["chrome"]).getRandom.get("useragent")
         options.add_argument(f"user-agent={user_agent}")
         return options
